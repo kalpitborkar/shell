765bd09a812e03e53a37a2eb04641d9d854538e1
kalpitborkar <81480133+kalpitborkar@users.noreply.github.com>
2022-11-24 16:51:55 +0530

    Add launch, execute and readline functions

--- src/shell.c
+++ src/shell.c
@@ -52,3 +52,71 @@ int shell_help(char **args){
 int shell_exit(char **args){
     return 0;
 }
+
+int shell_launch(char **args){
+    pid_t pid;
+    int status;
+    pid = fork();
+    if(pid == 0){
+        if(execvp(args[0], args) == -1){
+            perror("shell");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if(pid < 0){
+        perror("shell");
+    }
+    else{
+        do{
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+}
+
+int shell_execute(char **args){
+    int i;
+    if(args[0] == NULL){
+        return 1;
+    }
+
+    for (i = 0; i < lsh_num_builtins(); i++){
+        if (strcmp(args[0], builtin_str[i]) == 0){
+            return (*builtin_func[i])(args);
+        }
+    }
+    return lsh_launch(args);
+}
+
+#define shell_RL_BUFSIZE 1024
+
+char *shell_read_line(void){
+    int bufsize = shell_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(*char) * bufsize);
+    int c;
+
+    if(!buffer){
+        fprintf(stderr, "\n\nshell: %s\n\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    while(1){
+        c = getchar();
+        if(c == EOF || c == '\n'){
+            buffer[position] = '\0';
+            return buffer;
+        }
+        else{
+            buffer[position] = c;
+        }
+        position++;
+        if(position > bufsize){
+            bufsize += shell_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if(!buffer){
+                fprintf(stderr, "\n\nshell: %s\n\n", strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}

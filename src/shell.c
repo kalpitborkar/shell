3072b6d1b233aaaa0acbfdf8aed5346cfbdf752e
kalpitborkar <81480133+kalpitborkar@users.noreply.github.com>
2022-11-24 13:16:32 +0530

    Define builtin functions

--- src/shell.c
+++ src/shell.c
@@ -27,3 +27,28 @@ int (*builtin_func[]) (char **) = {
 int shell_num_builtins(){
     return sizeof(builtin_str)/sizeof(char *);
 }
+
+int shell_cd(char **args){
+    if(args[1] == NULL){
+        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    }
+    else{
+        if(chdir(args[1]) != 0){
+            perror("shell");
+        }
+    }
+    return 1;
+}
+
+int shell_help(char **args){
+    int i;
+    printf("The following are built in:\n");
+    for(i = 0; i < shell_num_builtins(); i++){
+        printf(" %s\n", builtin_str[i]);
+    }
+    return 1;
+}
+
+int shell_exit(char **args){
+    return 0;
+}
